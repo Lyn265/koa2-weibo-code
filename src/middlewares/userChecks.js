724fa5a19e8595962ea5d4a0dbc408ca993e51ea
Lyn265 <chenglliu2018@gmail.com>
2021-09-11 22:23:47 +0900

    fact:登录抽离中间件

---
+++ src/middlewares/userChecks.js
@@ -0,0 +1,30 @@
+const { ErrorModel } = require('../model/ResModel')
+const {loginCheckFailInfo} = require('../model/ErrorInfo')
+const { ne } = require('sequelize/types/lib/operators')
+/**
+ * @description API登录验证
+ * @param {*} ctx 
+ * @param {*} next 
+ * @returns 
+ */
+async function loginCheck(ctx,next){
+    if(ctx.session && ctx.session.userInfo){
+        await next()
+        return 
+    }
+    return new ErrorModel(loginCheckFailInfo)
+}
+
+async function loginRedirect(ctx,next){
+    if(ctx.session && ctx.session.userInfo){
+        await next()
+        return 
+    }
+    const url = ctx.url
+    ctx.redirect('/login?url='+ encodeURIComponent(url))
+}
+
+module.exports={
+    loginCheck,
+    loginRedirect
+}
--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -1,11 +1,27 @@
 const router = require('koa-router')()
 
+/**
+ * 
+ * @param {object} ctx 
+ */
+function getLoginInfo(ctx){
+    let data = {
+        isLogin:false
+    }
+    if(ctx.session.userInfo){
+        data = {
+            isLogin:true,
+            userName:ctx.session.userInfo.userName
+        }
+    }
+    return data
+}
 router.get('/register',async(ctx,next) =>{
-    await ctx.render('register',{})
+    await ctx.render('register',getLoginInfo(ctx))
 })
 
 router.get('/login',async(ctx,next) =>{
-    await ctx.render('login',{})
+    await ctx.render('login',getLoginInfo(ctx))
 })
 
 
